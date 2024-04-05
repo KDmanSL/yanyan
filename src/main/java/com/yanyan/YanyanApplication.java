badb7e98da615f3fc93d7bf5ca96988f5ddf85fb
韶光善良君 <kindness-man@outlook.com>
2024-04-05 17:37:46 +0800

    测试连接

--- src/main/java/com/yanyan/YanyanApplication.java
+++ src/main/java/com/yanyan/YanyanApplication.java
@@ -1,9 +1,12 @@
 package com.yanyan;
 
+import org.apache.ibatis.annotations.Mapper;
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@MapperScan("com.yanyan.mapper")
 public class YanyanApplication {
 
 	public static void main(String[] args) {
--- src/main/java/com/yanyan/controller/CourseController.java
+++ src/main/java/com/yanyan/controller/CourseController.java
@@ -1,2 +1,18 @@
-package com.yanyan.controller;public class CourseController {
+package com.yanyan.controller;
+
+import com.yanyan.dto.Result;
+import com.yanyan.service.CourseService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+@RequestMapping(value = "/course")
+@RestController
+public class CourseController {
+    @Autowired
+    CourseService courseService;
+
+    @GetMapping(value = "/{id}")
+    public Result queryCourseById(@PathVariable("id") Long id){
+        return courseService.queryCourseById(id);
+    }
 }
