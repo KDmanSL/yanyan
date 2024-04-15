1773a630f48bbe04353058704fd208520b8e27df
韶光善良君 <kindness-man@outlook.com>
2024-04-16 01:09:37 +0800

    合并格式，统一配置文件yml

--- src/main/java/com/yanyan/controller/AIController.java
+++ src/main/java/com/yanyan/controller/AIController.java
@@ -1,22 +1,23 @@
-package com.yanyan.ai;
+package com.yanyan.controller;
 
+import jakarta.annotation.Resource;
 import org.springframework.ai.chat.ChatClient;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Map;
 
 @RestController
-public class AIContoller {
+@RequestMapping("/ai")
+public class AIController {
 
-    @Autowired
+    @Resource
     ChatClient chatClient;
 
-    @GetMapping("/ai/simple")
+    @GetMapping("/simple")
     public Map<String, String> completion(@RequestParam(value = "message", defaultValue = "给我讲个笑话") String message) {
-
         var value=chatClient.call(message);
         System.out.println(value);
         return Map.of("generation",value );
