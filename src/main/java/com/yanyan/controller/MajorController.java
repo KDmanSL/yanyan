ab871471e01f286ef047752c9d85d13c7234e8f7
韶光善良君 <kindness-man@outlook.com>
2024-05-03 16:15:11 +0800

    修正public key retrieval is not allowed mysql问题

--- src/main/java/com/yanyan/controller/MajorController.java
+++ src/main/java/com/yanyan/controller/MajorController.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @Slf4j
-@RequestMapping(value = "/course")
+@RequestMapping(value = "/major")
 @CrossOrigin
 @RestController
 public class MajorController {
