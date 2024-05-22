c133cad9ac626e91d1adf2d013bdf56a1383a4f4
韶光善良君 <kindness-man@outlook.com>
2024-05-22 23:34:52 +0800

    学生role拦截器实现

--- src/main/java/com/yanyan/utils/PermissionInterceptor.java
+++ src/main/java/com/yanyan/utils/PermissionInterceptor.java
@@ -9,11 +9,6 @@ public class PermissionInterceptor implements HandlerInterceptor {
 
     @Override
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
-//        UserDTO user = UserHolder.getUser();
-//        // 当用户未登录的时候放行，因为在此之前存在登录拦截器，对一些不敏感页面不需要拦截
-//        if (user == null) {
-//            return true;
-//        }
 
         String role = UserHolder.getUser().getRole();
         return !role.equals("stu");
