359553d8d44a40dd07d19b7f78cd0219d9917e23
韶光善良君 <kindness-man@outlook.com>
2024-05-23 15:47:22 +0800

    解决option请求拦截问题

--- src/main/java/com/yanyan/utils/LoginInterceptor.java
+++ src/main/java/com/yanyan/utils/LoginInterceptor.java
@@ -2,12 +2,19 @@ package com.yanyan.utils;
 
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.http.HttpMethod;
 import org.springframework.web.servlet.HandlerInterceptor;
 
 public class LoginInterceptor implements HandlerInterceptor {
 
     @Override
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+        //如果请求为 OPTIONS 请求，则返回 true,否则需要通过jwt验证
+        if (HttpMethod.OPTIONS.toString().equals(request.getMethod())){
+            System.out.println("OPTIONS请求，放行");
+            return true;
+        }
+
         //判断ThreadLocal中有无用户
         if(UserHolder.getUser()==null){
             response.setStatus(401);
--- src/main/java/com/yanyan/utils/PermissionInterceptor.java
+++ src/main/java/com/yanyan/utils/PermissionInterceptor.java
@@ -3,12 +3,18 @@ package com.yanyan.utils;
 import com.yanyan.dto.UserDTO;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.http.HttpMethod;
 import org.springframework.web.servlet.HandlerInterceptor;
 
 public class PermissionInterceptor implements HandlerInterceptor {
 
     @Override
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+        //如果请求为 OPTIONS 请求，则返回 true,否则需要通过jwt验证
+        if (HttpMethod.OPTIONS.toString().equals(request.getMethod())){
+            System.out.println("OPTIONS请求，放行");
+            return true;
+        }
 
         String role = UserHolder.getUser().getRole();
         return !role.equals("stu");
