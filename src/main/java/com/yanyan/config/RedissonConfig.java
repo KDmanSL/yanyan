5160709d1df1c92b0ff3960b9059ca78446c4d8d
韶光善良君 <134353875+KDmanSL@users.noreply.github.com>
2024-06-02 22:46:02 +0800

    Update RedissonConfig.java

--- src/main/java/com/yanyan/config/RedissonConfig.java
+++ src/main/java/com/yanyan/config/RedissonConfig.java
@@ -3,16 +3,23 @@ package com.yanyan.config;
 import org.redisson.Redisson;
 import org.redisson.api.RedissonClient;
 import org.redisson.config.Config;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class RedissonConfig {
+    @Value("${Spring.data.redis.host}")
+    private String host;
+    @Value("${Spring.data.redis.password}")
+    private String password;
+    @Value("${Spring.data.redis.database}")
+    private int database;
     @Bean
     public RedissonClient redissonClient() {
         // 配置
         Config config = new Config();
-        config.useSingleServer().setAddress("redis://119.3.158.9:6379").setPassword("redis").setDatabase(4);
+        config.useSingleServer().setAddress("redis://"+host+":6379").setPassword(password).setDatabase(database);
 
         return Redisson.create(config);
     }
